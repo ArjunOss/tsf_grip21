0dc307d0822627f426a7ebc3640eca3440078822
ArjunOss <arjunkumar.cse@outlook.com>
2021-11-20 19:28:26 +0530

    db_conn

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "id17904637_bank_db";
+$password = "8g0{<Nf9x7Pq*+p4";
+$dbname = "id17904637_bankdb";
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if($conn){
+    // echo "connection ok";
+}
+else{
+    // echo "connection not ok";
+}
+
+?>
